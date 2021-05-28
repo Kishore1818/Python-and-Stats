402abdaffb3332434c3a27c417ff9099a2bce299
Kishore: Machine Learning and Data Scientist <kishore1818@gmail.com>
2021-05-28 16:06:39 -0700

    Add files via upload

---
+++ Week8/extract_features.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 28 11:18:53 2021
+
+@author: kishore
+"""
+
+from sklearn.metrics import precision_recall_fscore_support
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction.text import CountVectorizer,TfidfVectorizer
+
+import numpy as np
+import logging
+
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+def extract_features(df,field,training_data,testing_data,type="binary"):
+    """Extract features using different methods"""
+    
+    logging.info("Extracting features and creating vocabulary...")
+    
+    if "binary" in type:
+        
+        # BINARY FEATURE REPRESENTATION
+        cv= CountVectorizer(binary=True, max_df=0.95)
+        cv.fit_transform(training_data[field].values)
+        
+        train_feature_set=cv.transform(training_data[field].values)
+        test_feature_set=cv.transform(testing_data[field].values)
+        
+        return train_feature_set,test_feature_set,cv
+  
+    elif "counts" in type:
+        
+        # COUNT BASED FEATURE REPRESENTATION
+        cv= CountVectorizer(binary=False, max_df=0.95)
+        cv.fit_transform(training_data[field].values)
+        
+        train_feature_set=cv.transform(training_data[field].values)
+        test_feature_set=cv.transform(testing_data[field].values)
+        
+        return train_feature_set,test_feature_set,cv
+    
+    else:    
+        
+        # TF-IDF BASED FEATURE REPRESENTATION
+        tfidf_vectorizer=TfidfVectorizer(use_idf=True, max_df=0.95)
+        tfidf_vectorizer.fit_transform(training_data[field].values)
+        
+        train_feature_set=tfidf_vectorizer.transform(training_data[field].values)
+        test_feature_set=tfidf_vectorizer.transform(testing_data[field].values)
+        
+        return train_feature_set,test_feature_set,tfidf_vectorizer
